f73fa17adc15b50a51d7cd2a446488afe1b45760
LilyAsFlora <acrylicrainbow1922@gmail.com>
2023-01-14 23:06:19 +0000

    Add unit tests for uppercase encryption

--- tests/test_encrypt.py
+++ tests/test_encrypt.py
@@ -13,6 +13,10 @@ class TestEncrypt(unittest.TestCase):
         """Negative single character encryption."""
         self.assertEqual(cipher.caesar_encrypt_char("a", -1), "z")
 
+    def test_encrypt_char_upper(self):
+        """Uppercase character encryption."""
+        self.assertEqual(cipher.caesar_encrypt_char("G", 3), "J")
+
     def test_encrypt_text_pos(self):
         """Positive text encryption."""
         self.assertEqual(cipher.caesar_encrypt_text("abc", 1), "bcd")
@@ -20,6 +24,10 @@ class TestEncrypt(unittest.TestCase):
     def test_encrypt_test_neg(self):
         """Negative text encryption."""
         self.assertEqual(cipher.caesar_encrypt_text("abc", -1), "zab")
+
+    def test_encrypt_text_upper(self):
+        """Uppercase text encryption."""
+        self.assertEqual(cipher.caesar_encrypt_text("FOO", 4), "JSS")
 
     def test_encrypt_text_round(self):
         """Test shift rounding."""
