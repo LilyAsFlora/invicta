39f83003e836d4b9fa112f195eba3c14422e7e44
LilyAsFlora <acrylicrainbow1922@gmail.com>
2023-01-14 21:11:42 +0000

    Add docstrings to output functions

--- output.py
+++ output.py
@@ -3,10 +3,12 @@
 import cipher
 
 def output_encryption(args):
+    """Handles encryption output for the given arguments."""
     result = cipher.caesar_encrypt_text(args.text, args.shift)
     print(result)
 
 def output_decryption(args):
+    """Handles decryption output for the given arguments."""
     result = cipher.caesar_decrypt_text(args.text)
 
     for string in result:
