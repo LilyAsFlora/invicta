cf6e718296d0bc7eedc5c96c06bbaf736f1cf5c3
LilyAsFlora <acrylicrainbow1922@gmail.com>
2023-01-15 13:42:10 +0000

    Add test_ to function name

--- tests/test_can_encrypt.py
+++ tests/test_can_encrypt.py
@@ -5,6 +5,6 @@ import unittest
 import cipher
 
 class TestCanEncrypt(unittest.TestCase):
-    def can_encrypt_valid(self):
+    def test_can_encrypt_valid(self):
         """Test for valid data."""
         self.assertTrue(cipher.can_caesar_encrypt("Hello, world!"))
