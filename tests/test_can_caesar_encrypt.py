5388e31612f9785e92778c9af682e9e030a9a145
LilyAsFlora <acrylicrainbow1922@gmail.com>
2023-01-15 14:30:56 +0000

    Update test function implmentations for can_caesar_encrypt() for readability

--- tests/test_can_caesar_encrypt.py
+++ tests/test_can_caesar_encrypt.py
@@ -6,16 +6,31 @@ import cipher
 
 class TestCanCaesarEncrypt(unittest.TestCase):
     def test_can_caesar_encrypt_valid_returns_true(self):
-        self.assertTrue(cipher.can_caesar_encrypt("Hello, world!"))
+        valid_plaintext = "Hello, world"
+        result = cipher.can_caesar_encrypt(valid_plaintext)
+
+        self.assertTrue(result)
 
     def test_can_encrypt_invalid_returns_false(self):
-        self.assertFalse(cipher.can_caesar_encrypt("293847"))
+        invalid_plaintext = "1"
+        result = cipher.can_caesar_encrypt(invalid_plaintext)
+
+        self.assertFalse(result)
 
     def test_can_encrypt_empty_returns_false(self):
-        self.assertFalse(cipher.can_caesar_encrypt(""))
+        empty_str = ""
+        result = cipher.can_caesar_encrypt(empty_str)
+
+        self.assertFalse(result)
 
     def test_can_encrypt_control_returns_false(self):
-        self.assertFalse(cipher.can_caesar_encrypt("\x01\x13\x07"))
+        control_char_str = "\x01\x13\x07"
+        result = cipher.can_caesar_encrypt(control_char_str)
+        
+        self.assertFalse(result)
 
     def test_can_encrypt_multi_returns_true(self):
-        self.assertTrue(cipher.can_caesar_encrypt("Hello 02384029384"))
+        mixed_str = "a1"
+        result = cipher.can_caesar_encrypt(mixed_str)
+
+        self.assertTrue(result)
