67e1a3442bb6869fa49d1432fa5d0bdcd2d5467a
LilyAsFlora <acrylicrainbow1922@gmail.com>
2023-01-15 14:26:02 +0000

    Update test function names for encryption

--- tests/test_encrypt.py
+++ tests/test_encrypt.py
@@ -4,47 +4,37 @@ import unittest
 
 import cipher
 
-class TestEncrypt(unittest.TestCase):
-    def test_encrypt_char_pos(self):
-        """Positive single character encryption."""
+class TestCaesarEncrypt(unittest.TestCase):
+    def test_caesar_encrypt_positive_char(self):
         self.assertEqual(cipher.caesar_encrypt_char("a", 1), "b")
 
-    def test_encrypt_char_neg(self):
-        """Negative single character encryption."""
+    def test_caesar_encrypt_negative_char(self):
         self.assertEqual(cipher.caesar_encrypt_char("a", -1), "z")
 
-    def test_encrypt_char_upper(self):
-        """Uppercase character encryption."""
+    def test_caesar_encrypt_char_upper_preserves_case(self):
         self.assertEqual(cipher.caesar_encrypt_char("G", 3), "J")
 
-    def test_encrypt_text_pos(self):
-        """Positive text encryption."""
+    def test_caesar_encrypt_positive_text(self):
         self.assertEqual(cipher.caesar_encrypt_text("abc", 1), "bcd")
 
-    def test_encrypt_text_neg(self):
-        """Negative text encryption."""
+    def test_caesar_encrypt_negative_text(self):
         self.assertEqual(cipher.caesar_encrypt_text("abc", -1), "zab")
 
-    def test_encrypt_text_upper(self):
-        """Uppercase text encryption."""
+    def test_caesar_encrypt_text_upper_preserves_case(self):
         self.assertEqual(cipher.caesar_encrypt_text("FOO", 4), "JSS")
 
-    def test_encrypt_text_round(self):
-        """Test shift rounding."""
+    def test_caesar_encrypt_fractional_shift_is_rounded(self):
         self.assertEqual(cipher.caesar_encrypt_text("a", 1.2), "b")
 
-    def test_encrypt_empty(self):
-        """Test encryption for an empty string."""
+    def test_caesar_encrypt_empty_string_returns_empty(self):
         self.assertEqual(cipher.caesar_encrypt_text("", 17), "")
 
-    def test_encrypt_non_ascii(self):
-        """Test encryption for non-ASCII characters."""
+    def def_test_caesar_encrypt_non_ascii_chars_preserved(self):
         test_data = "→⁷∴°"
 
         self.assertEqual(cipher.caesar_encrypt_text(test_data, 14), test_data)
 
-    def test_encrypt_control(self):
-        """Test encryption for control characters."""
+    def test_caesar_encrypt_control_chars_preserved(self):
         test_data = "\x16\x1E\x7F"
 
         self.assertEqual(cipher.caesar_encrypt_text(test_data, 15), test_data)
