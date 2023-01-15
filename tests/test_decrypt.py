68eaeb14f32d1dca768f92338bb6c1d9ae30aede
LilyAsFlora <acrylicrainbow1922@gmail.com>
2023-01-15 14:26:58 +0000

    Update test function names for decryption

--- tests/test_decrypt.py
+++ tests/test_decrypt.py
@@ -5,13 +5,12 @@ import unittest
 import cipher
 
 class TestDecrypt(unittest.TestCase):
-    def test_decrypt_single(self):
-        """Test for a single word."""
+    def test_caesar_decrypt_word_in_plaintexts(self):
         encrypted = cipher.caesar_encrypt_text("foo", 16)
 
         self.assertIn("foo", cipher.caesar_decrypt_text(encrypted))
 
-    def test_decrypt_multi(self):
+    def test_decrypt_words_in_plaintexts(self):
         """Test for multiple words."""
         encrypted = cipher.caesar_encrypt_text("Hello, world!", 25)
 
