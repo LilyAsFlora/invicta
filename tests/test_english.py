e100a9f54f40aac27478b293fd617fdf67044348
LilyAsFlora <acrylicrainbow1922@gmail.com>
2023-01-16 22:06:45 +0000

    Define test for is_english_word() for valid data

--- tests/test_english.py
+++ tests/test_english.py
@@ -2,7 +2,16 @@
 
 import unittest
 
+import spellchecker
+
 import english
 
 class TestEnglish(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.spell_checker = spellchecker.SpellChecker()
+
+    def test_is_english_word_true_for_english(self):
+        test_word = "the"
+        result = english.is_english_word(test_word, self.spell_checker)
+
+        self.assertTrue(result)
