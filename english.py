3662f50f77d2c0b7d60b68a4e23dce6d7f79c1ec
LilyAsFlora <acrylicrainbow1922@gmail.com>
2023-01-16 22:02:05 +0000

    Add function contains_english() for string-wide English checks

--- english.py
+++ english.py
@@ -14,3 +14,17 @@ def is_english_word(word: str, checker: SpellChecker) -> bool:
         checker: An instance of pyspellchecker.Spellchecker
     """
     return word in checker.known([word])
+
+def contains_english(string: str, checker: SpellChecker) -> bool:
+    """Returns `True` if the given string contains a word that exists in the
+    English dictionary.
+    
+    See `is_english_word` for the definition of a `word`.
+
+    Args:
+        word: A string to test. This does not have to be pre-sanitised.
+        checker: An instance of pyspellchecker.Spellchecker
+    """
+    return any(
+        [is_english_word(word.strip(), checker) for word in string.split(" ")]
+    )
